bbb3b57fa07905fcaac150b9af84ed49b9b7e42f
mozinomer <mozinomer12@gmail.com>
2021-03-09 12:00:21 +0500

    sd

--- footer.php
+++ footer.php
@@ -1,79 +1,68 @@
-<?php wp_footer(); ?>
+
 <?php if ( ! is_front_page() ) { ?>
-        <div class="news-contct">
-            <div class="container">
-                <div class="inner-news">
-                <div class="iner-width">
-                <p><?php the_field('contac_heading'); ?></p>
-                 <h4><?php the_field('conatct_para'); ?></h4>
-                 <span><?php the_field('contct_subhead'); ?></span>
-                 </div>
-                 <div class="iner-width1">
-                 <div class="cin">
-                 <a href="<?php the_field('contact_link'); ?>">Contact Us Today <img src="http://localhost/janus/wp-content/uploads/2021/03/right-arow.png"></a>
-                 </div>
-                 </div>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
-    <?php if ( ! is_front_page() ) { ?>
-        <div class="contactusBeforeFooter">
-            <div class="container">
-                <div class="containerCOntactBeforeFooter">
-                    <h6><?php the_field('asds'); ?></h6>
-                    <h3><?php the_field('asds'); ?></h3>
-                    <p><?php the_field('asds'); ?></p>
-                </div>
-                <a href="<?php echo site_url(); ?>/contact-us"><?php the_field('asds'); ?></a>
-            </div>
-        </div>
-    <?php } ?>
-    <footer>
+    <div class="news-contct">
         <div class="container">
-            <div class="topContainer">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="aboutcontentFotoer">
-                            <h6>About company</h6>
-                            <p>To use our Big Enterprise experience to leverage cutting-edge technologies and methodologies to deliver successful solutions for small- and medium-sized businesses, (and funded startups) leading to groundbreaking success.  </p>
-                            <ul>
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            </ul>
-                        </div>
+            <div class="inner-news">
+                <div class="iner-width">
+                    <h6><?php the_field('headingcn', 'options'); ?></h6>
+                <h3><?php the_field('sub_headingcn', 'options'); ?></h3>
+                <p><?php the_field('contentcn', 'options'); ?></p>
+                </div>
+                <div class="iner-width1">
+                   <div class="cin">
+                       <a href="<?php the_field('contact_link'); ?>">Contact Us Today <img src="http://localhost/janus/wp-content/uploads/2021/03/right-arow.png"></a>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+<?php } ?>
+<footer>
+    <div class="container">
+        <div class="topContainer">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="aboutcontentFotoer">
+                        <h6>About company</h6>
+                        <p>To use our Big Enterprise experience to leverage cutting-edge technologies and methodologies to deliver successful solutions for small- and medium-sized businesses, (and funded startups) leading to groundbreaking success.  </p>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
                     </div>
-                    <div class="col-md-4">
-                        <div class="listConyainer">
-                            <h6>Quick Links</h6>
-                            <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Why Us</a></li>
-                                <li><a href="#">Solutions   </a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="#">Pricing</a></li>
-                                <li><a href="#">FAQ's</a></li>
-                                <li><a href="#">Blogs</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                            </ul>
-                        </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="listConyainer">
+                        <h6>Quick Links</h6>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Why Us</a></li>
+                            <li><a href="#">Solutions   </a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">FAQ's</a></li>
+                            <li><a href="#">Blogs</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
                     </div>
-                    <div class="col-md-4">
-                        <div class="lastCOlumnSectionFOoter">
-                            <h6>Learn More</h6>
-                            <p>Download our brochure to learn more about the company.</p>
-                            <a href="#" class="downloadFile"><img src="./images/6.svg">Download Brochure</a>
-                            <div class="emailUs">
-                                <h5>Email Us Now!</h5>
-                                <a href="mailto:info@janusconsultingpartners.com"> info@janusconsultingpartners.com</a>
-                            </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="lastCOlumnSectionFOoter">
+                        <h6>Learn More</h6>
+                        <p>Download our brochure to learn more about the company.</p>
+                        <a href="#" class="downloadFile"><img src="<?php echo get_template_directory_uri(); ?>/images/6.svg">Download Brochure</a>
+                        <div class="emailUs">
+                            <h5>Email Us Now!</h5>
+                            <a href="mailto:info@janusconsultingpartners.com"> info@janusconsultingpartners.com</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" integrity="sha512-gY25nC63ddE0LcLPhxUJGFxa2GoIyA5FLym4UJqHDEMHjp8RET6Zn/SHo1sltt3WuVtqfyxECP38/daUc/WVEA==" crossorigin="anonymous"></script>
- <!-- custom js -->
- <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+<!-- custom js -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
--- solutionsTemplate.php
+++ solutionsTemplate.php
@@ -32,49 +32,49 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="contentSection3Solutions">
-					<h2><?php the_field('ssd'); ?></h2>
-					<p><?php the_field('ssd'); ?></p>
+					<h2><?php the_field('headingss'); ?></h2>
+					<p><?php the_field('contentss'); ?></p>
 					<ul>
-						<?php if( have_rows('repeater_field_name') ): while( have_rows('repeater_field_name') ) : the_row(); ?>
+						<?php if( have_rows('listss') ): while( have_rows('listss') ) : the_row(); ?>
 							<li>
 								<img src="<?php echo get_template_directory_uri(); ?>/images/5.png">
-								<?php the_sub_field('asd'); ?>
+								<?php the_sub_field('list_itemsss'); ?>
 							</li>
 						<?php endwhile; endif; ?>
 					</ul>
 
 					<ol>
-						<?php if( have_rows('repeater_field_name') ): while( have_rows('repeater_field_name') ) : the_row(); ?>
+						<?php if( have_rows('inner_list') ): while( have_rows('inner_list') ) : the_row(); ?>
 							<li>
-								- <?php the_sub_field('asd'); ?>
+								- <?php the_sub_field('inner_list_itemss'); ?>
 							</li>
 						<?php endwhile; endif; ?>
 					</ol>
 					<p class="darker">
-						<?php the_field('asd'); ?>
+						<?php the_field('strong_textsss'); ?>
 					</p>
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="imgeContainersectionSoluton">
-					<img src="<?php the_field('imageContainerSection3Solution'); ?>">
+					<img src="<?php the_field('imagess'); ?>">
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="section4">
-	<?php if( have_rows('repeater_field_name') ): while( have_rows('repeater_field_name') ) : the_row(); ?>
+	<?php if( have_rows('row_repeater') ): while( have_rows('row_repeater') ) : the_row(); ?>
 		<div class="rowContainer">
 			<div class="col-1s">
-				<img src="<?php the_sub_field('asd'); ?>">
+				<img src="<?php the_sub_field('imagesd'); ?>">
 			</div>
 			<div class="cols2">
 				<div class="contentContainercolumsn">
-					<h6><?php the_field('asds'); ?></h6>
-					<h2><?php the_field('asds'); ?></h2>
-					<p><?php the_field('asds'); ?></p>
-					<strong><?php the_field('asds'); ?></strong>
+					<h6><?php the_sub_field('mini_heading'); ?></h6>
+					<h2><?php the_sub_field('headingsd'); ?></h2>
+					<p><?php the_sub_field('contentsd'); ?></p>
+					<strong><?php the_sub_field('strong_text'); ?></strong>
 				</div>
 			</div>
 		</div>
